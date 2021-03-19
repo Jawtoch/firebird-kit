406ce0d1e6761d6b75cde4898b230424d1f828da
Ugo Cottin <ugo.cottin@gmail.com>
2021-03-20 00:49:36 +0100

    ajout de la SQLDatabase

--- Sources/FirebirdKit/SQL/FirebirdSQLDatabase.swift
+++ Sources/FirebirdKit/SQL/FirebirdSQLDatabase.swift
@@ -5,11 +5,36 @@
 //  Created by Ugo Cottin on 19/03/2021.
 //
 
-public protocol FirebirdSQLDatabase {
+public struct FirebirdSQLDatabase {
 	
-	var database: FirebirdDatabase { get }
+	let database: FirebirdDatabase
 	
-	var encoder: FirebirdEncoder { get }
+	let encoder: FirebirdEncoder = FirebirdEncoder()
 	
-	var decoder: FirebirdDecoder { get }
+	let decoder: FirebirdDecoder
+}
+
+extension FirebirdSQLDatabase: SQLDatabase {
+	public var logger: Logger { self.database.logger }
+	
+	public var eventLoop: EventLoop { self.database.eventLoop }
+	
+	public var dialect: SQLDialect { FirebirdDialect( )}
+	
+	public func execute(sql query: SQLExpression, _ onRow: @escaping (SQLRow) -> ()) -> EventLoopFuture<Void> {
+		let (sql, binds) = self.serialize(query)
+
+		do {
+			let firebirdBinds = try binds.map { try self.encoder.encode($0) }
+			return self.database.query(
+				sql,
+				firebirdBinds,
+				onMetadata: { print("Metadata: \($0)") },
+				onRow: { print("Row: \($0)") })
+				.map { _ in }
+			
+		} catch {
+			return self.eventLoop.makeFailedFuture(error)
+		}
+	}
 }
