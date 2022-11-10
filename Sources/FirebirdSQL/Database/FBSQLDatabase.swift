6d5ee487ca6bff296b68d7c57dedd9391a6e2789
Ugo Cottin <ugo.cottin@ekonoo.com>
2022-11-10 19:39:42 +0100

    fix limit and offset in query serialized by SQLKit

--- Sources/FirebirdSQL/Database/FBSQLDatabase.swift
+++ Sources/FirebirdSQL/Database/FBSQLDatabase.swift
@@ -13,7 +13,40 @@ public struct FBSQLDatabase: FirebirdSQLDatabase {
     }
     
     public func execute(sql query: SQLExpression, _ onRow: @escaping (SQLRow) -> ()) -> EventLoopFuture<Void> {
-        let (sql, binds) = self.serialize(query)
+        var (sql, binds) = self.serialize(query)
+        
+        let limitRange = sql.range(of: #" LIMIT (\d+)"#, options: .regularExpression)
+        
+        let offsetRange = sql.range(of: #" OFFSET (\d+)"#, options: .regularExpression)
+        
+        var limit: Int? = nil
+        
+        var offset: Int? = nil
+        
+        if let offsetRange {
+            let offsetStatement = sql[offsetRange]
+            let valueRange = offsetStatement.range(of: #"\d+"#, options: .regularExpression)!
+            let stringValue = offsetStatement[valueRange]
+            offset = Int(stringValue)!
+            
+            sql.removeSubrange(offsetRange)
+        }
+        
+        if let limitRange {
+            let limitStatement = sql[limitRange]
+            let valueRange = limitStatement.range(of: #"\d+"#, options: .regularExpression)!
+            let stringValue = limitStatement[valueRange]
+            limit = Int(stringValue)!
+            
+            sql.removeSubrange(limitRange)
+        }
+                
+        if let limit {
+            let offset = offset ?? 0
+            let startIndex = offset + 1
+            let endIndex = offset + limit
+            sql.append(" ROWS \(startIndex) TO \(endIndex)")
+        }
         
         return self.database.query(sql, binds, onRow: onRow)
     }
